eb6de022dd0ed910cf68675a242f1ebbd645b324
Ashish Popandiya <108505051+dhakad19@users.noreply.github.com>
2022-07-01 21:24:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+ //name=prompt('what is your name?')
+function click1(){
+    window.open('https://www.bitdegree.org/tutorials/how-to-start-coding/', '_blank');
+    
+}
+function about(){
+    window.open('https://auth.geeksforgeeks.org/user/popandiyaashish/profile','_blank');
+}
+function click2(){
+    window.open('https://www.tutorialspoint.com/effective_resume_writing.htm','_blank');
+}
+function click3(){
+    window.open('https://medium.com/javarevisited/top-25-amazon-sde-interview-questions-cfe0ef70ba9e','_blank');
+}
+function helfull(){
+    swal({
+        title: `Thanks ${name}`,
+        text: "Visit again!!",
+        icon: "success",
+    })
+}
+function nhelfull(){
+    swal({
+        title: "Oops!",
+        text: "Give feedback",
+        icon: "warning",
+    })
+}
+   
+
+
